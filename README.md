1f8c3ec00eba9c89539e36e99fd63d9a0a26c47e
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-01-22 04:20:19 +0530

    project init, added custom fonts, installed dependencies

---
+++ README.md
@@ -0,0 +1,13 @@
+# React-Netflixx
+
+### Custom Font Setup
+
+- Add Font file - Assets/Fonts/font files
+- Add font-faces in index.css file
+- initilise font property in tailwind config file like, this
+  `extend: {
+    fontFamily: {
+    "nsans-light": ["Nsans Light"],
+"nsans-medium": ["Nsans Medium"]
+}
+}`
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='uppercase font-nsans-light  text-white'>Netflix</div>
+      <div className='uppercase font-nsans-regular text-white'>Netflix</div>
+      <div className='uppercase font-nsans-medium  text-white'>Netflix</div>
+      <div className='uppercase font-nsans-bold text-white'>Netflix</div>
+
+    </div>
+  )
+}
+
+export default App
