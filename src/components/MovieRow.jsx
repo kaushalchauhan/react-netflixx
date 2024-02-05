10ef4780f46fed5adee7cb45978b940f74ffd7a4
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-02-05 17:13:31 +0530

    feat: slider scroll

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,27 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import MovieItem from "./MovieItem";
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
 
-const MovieRow = ({title,url}) => {
+const MovieRow = ({ title, url }) => {
   const [movies, setMovies] = useState([]);
-  useEffect(()=>{
-    axios.get(url).then((res)=>{
+  const rowId = Math.floor(Math.random() * 1000)
+  useEffect(() => {
+    axios.get(url).then((res) => {
       setMovies(res.data.results)
     })
-  },[url])
+  }, [url])
 
+  const slide = (offset) => {
+    const slider = document.getElementById("slider" + rowId)
+    slider.scrollLeft += offset;
+  }
 
   return (
     <>
-    <h2 className="font-nsans-bold md:text-xl p-4 capitalize">{title}</h2>
-    <div className="relative flex items-center">
-      <div id={`slider`} className="h-full w-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
-        {movies.map((movie)=>(
-          <MovieItem key={movie.id} movie={movie}/>
-        ))}
+      <h2 className="font-nsans-bold md:text-xl p-4 capitalize">{title}</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft onClick={() => slide(-500)} className="bg-white rounded-full absolute left-2 opacity-80 text-gray-700 z-10 hidden group-hover:block cursor-pointer" size={40} />
+        <div id={`slider` + rowId} className="h-full w-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} />
+          ))}
+        </div>
+        <MdChevronRight onClick={() => slide(500)} className="bg-white rounded-full absolute right-2 opacity-80 text-gray-700 z-10 hidden group-hover:block cursor-pointer" size={40} />
       </div>
-    </div>
     </>
   )
 }
